f5d3eec090f2ef8e378413334283346f86544340
Eliezer Benjamin <eliezerben96@gmail.com>
2024-02-25 23:49:51 +0530

    Add option to specify output file. Minor improvements.

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import nltk
 from nltk.corpus import wordnet
 
 
-# CORPUS = nltk.corpus.gutenberg
-CORPUS = nltk.corpus.words
+CORPUS = nltk.corpus.gutenberg
+# CORPUS = nltk.corpus.words
 
 
 def get_wordnet_pos(treebank_tag):
@@ -33,7 +33,7 @@ def get_clean_words(words):
     '''Remove words containing only numbers, punctuations'''
     clean_words = []
 
-    # Matches strings containing only digits and punctuations(with leading, trailing spaces)
+    # Matches strings containing only digits/punctuations(with leading, trailing spaces)
     num_punct_re = re.compile(fr'\s*[0-9{re.escape(string.punctuation)}]+\s*$')
 
     for word, pos in words:
@@ -44,25 +44,27 @@ def get_clean_words(words):
     return clean_words
 
 
-def main(transcript_file_path):
+def main(transcript_file_path: Path, output_file_path: Path):
+    '''Write words to output file which are not in the corpus.
+    Compare lemmatized words also to get better accuracy'''
     lemmatizer = nltk.stem.WordNetLemmatizer()
     dict_words = set([w.lower() for w in CORPUS.words()])
     text = transcript_file_path.read_text(encoding='utf-8')
 
     non_english_words = set()
 
-    for sent in nltk.tokenize.sent_tokenize(text):
-        sent_words = nltk.tokenize.word_tokenize(sent)
-        words_with_pos = nltk.pos_tag(sent_words)
+    for sentence in nltk.tokenize.sent_tokenize(text):
+        words = nltk.tokenize.word_tokenize(sentence)
+        words_with_pos = nltk.pos_tag(words)
         clean_words_with_pos = get_clean_words(words_with_pos)
         for word, pos in clean_words_with_pos:
             wordnet_pos = get_wordnet_pos(pos)
-            lemma = lemmatizer.lemmatize(word, wordnet_pos).lower()
-            if word not in dict_words and lemma not in dict_words:
+            lemma = lemmatizer.lemmatize(word, wordnet_pos)
+            if word.lower() not in dict_words and lemma.lower() not in dict_words:
                 non_english_words.add(lemma)
 
-
-    Path('temp_words.txt').write_text('\n'.join(non_english_words))
+    output_file_path.parent.mkdir(exist_ok=True)
+    output_file_path.write_text('\n'.join(non_english_words))
 
 
 def get_args():
@@ -70,12 +72,21 @@ def get_args():
     arg_parser.add_argument(
         'transcript_path',
         type=Path,
-        required=True,
         help='Path to transcript file'
+    )
+    arg_parser.add_argument(
+        '-o',
+        '--output',
+        type=Path,
+        default=Path('./output.txt'),
+        help='Path to output file. Default: ./output.txt'
     )
     return arg_parser.parse_args()
 
 
 if __name__ == '__main__':
     args = get_args()
-    main(args.transcript_path)
+    main(
+        transcript_file_path=args.transcript_path,
+        output_file_path=args.output
+    )
